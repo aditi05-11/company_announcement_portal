f80592da0f8c91256acfb317043d45820da73332
aditi05-11 <aditibiramane08@gmail.com>
2024-07-18 23:18:00 +0530

    added validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :bio, presence: true
   validates :date_of_birth, presence: true
+  validates :password, presence: true, on: :create
 
 
 end
