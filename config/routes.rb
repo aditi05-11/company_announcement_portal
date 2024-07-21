4463e8353d7768e6304e16d89b8e6c225439014c
aditi05-11 <aditibiramane08@gmail.com>
2024-07-21 22:31:32 +0530

    added routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
   
 end
