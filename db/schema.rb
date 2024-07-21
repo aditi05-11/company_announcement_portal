3ca98d98571b3890b603069811b3ad40ca23b1dc
aditi05-11 <aditibiramane08@gmail.com>
2024-07-21 15:33:48 +0530

    updated schema for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_071612) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_181520) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "bio"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_18_071612) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
