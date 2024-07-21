85fa40d459a1423ee210d2c9461016dff73ac31a
aditi05-11 <aditibiramane08@gmail.com>
2024-07-21 22:32:59 +0530

    added schema for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_181520) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_21_101555) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.text "content"
+    t.integer "parent_comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.string "image"
@@ -31,5 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_18_181520) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
