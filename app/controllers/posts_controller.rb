ac20b770a0058e4daa90980343132b3b36755966
aditi05-11 <aditibiramane08@gmail.com>
2024-07-21 15:32:36 +0530

    created post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :require_login, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Post created successfully."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :image)
+  end
+end
