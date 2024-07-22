596c797c647ed0824ebe69ddbd284a42dba58bb2
aditi05-11 <aditibiramane08@gmail.com>
2024-07-22 08:09:56 +0530

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Clear existing data
+User.destroy_all
+Post.destroy_all
+
+user1 = User.create!(
+  name: "Sayali More",
+  bio: "Passionate developer.",
+  email: "sayali@example.com",
+  date_of_birth: "1990-01-01",
+  password: "password"
+)
+
+user2 = User.create!(
+  name: "Sampada Deshmukh",
+  bio: "I am a frontend developer.",
+  email: "sampada@example.com",
+  date_of_birth: "1985-05-15",
+  password: "password"
+)
+
+user3 = User.create!(
+  name: "Aditi Biramane",
+  bio: "Loves hiking.",
+  email: "aditi@example.com",
+  date_of_birth: "1978-12-25",
+  password: "password"
+)
+
+post1 = Post.create!(
+  content: "This is my first post! Excited to join this platform.",
+  user: user1
+)
+
+post2 = Post.create!(
+  content: "Had a great day!.",
+  user: user2
+)
+
+post3 = Post.create!(
+  content: "love to read books.",
+  user: user3
+)
+puts "Seeding completed successfully."
